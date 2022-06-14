46478af7efa70e927327badd8a884c4c6414ae19
Prabhjot Singh <pjsingh1359@gmail.com>
2022-06-14 15:51:54 -0700

    finished tutorial

---
+++ src/components/DragDrop.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useDrop } from 'react-dnd/dist/hooks';
+
+import Picture from './Picture';
+import '../App.css';
+
+const PictureList = [
+  {
+    id: 1,
+    url: require('../assets/forest1.jpg')
+  },
+  {
+    id: 2,
+    url: require("../assets/forest2.jpg")
+  },
+  {
+    id: 3,
+    url: require("../assets/forest3.jpg")
+  },
+]
+
+function DragDrop(props) {
+  const [board, setBoard] = useState([])
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'image',
+    drop: (item) => addImageToBoard(item.id),
+    collect: monitor => ({
+      isOver: !!monitor.isOver(),
+    })
+  }))
+
+  const addImageToBoard = (id) => {
+    const filteredPL = PictureList.filter(picture => id === picture.id)
+    setBoard((board) => [...board, filteredPL[0]]);
+  }
+  return (
+    <>
+      <div className='Pictures'>{PictureList.map(picture => {
+        return <Picture id={picture.id} url={picture.url} />
+      })}</div>
+      <div className='Board' ref={drop}>{board.map((picture) => {
+        return <Picture id={picture.id} url={picture.url} />
+      })}</div>
+    </>
+  );
+}
+
+export default DragDrop;
